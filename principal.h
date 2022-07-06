aad392940f5a7f5881b34748112cedc97e40bb2b
Geovanny Jaramillo <byronjaramillo_@hotmail.com>
2022-07-06 13:01:13 -0500

    Agreagdas barras inferior y barra derecha

--- principal.h
+++ principal.h
@@ -2,7 +2,11 @@
 #define PRINCIPAL_H
 
 #include <QMainWindow>
-
+#include <QMessageBox>
+#include <QPainter>
+#include <QFileDialog>
+#include <QDebug>
+#include <QString>
 QT_BEGIN_NAMESPACE
 namespace Ui { class Principal; }
 QT_END_NAMESPACE
@@ -15,15 +19,24 @@ public:
     Principal(QWidget *parent = nullptr);
     ~Principal();
 
+protected:
+    void paintEvent(QPaintEvent *event) override;
+
 private slots:
     void on_actionGuardar_triggered();
 
-    void on_pushButton_clicked(bool checked);
+    void on_inNota1_valueChanged(int arg1);
+
+    void on_inNota2_valueChanged(int arg1);
+
+    void on_inNota3_valueChanged(int arg1);
 
 private:
     Ui::Principal *ui;
     QPixmap lienzo;
 
     void dibujar();
+    int getAlto(int valor);
+    int incY(int alto); //incremento en Y
 };
 #endif // PRINCIPAL_H
